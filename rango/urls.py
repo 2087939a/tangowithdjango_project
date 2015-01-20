30909c68e1b9f78d3b1e56cfc960eb0aa9969f15
Terence Aqachmar <2087939a@student.gla.ac.uk>
2015-01-20 18:20:53 +0000

    Chapter 5 completed

--- rango/urls.py
+++ rango/urls.py
@@ -1,8 +1,16 @@
 from django.conf.urls import patterns, url
 from rango import views
-from rango import about
-
+from django.conf import settings # 5.3
+from django.conf.urls.static import static # 5.3
 
 urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
-        url(r'^about/', about.index ,name='index'))
+        url(r'^about/', views.about ,name='name'))
+
+		
+
+if not settings.DEBUG:
+        urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+		
+		
+		
