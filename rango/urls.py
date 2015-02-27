7c4b2ecbf4d35bc4de93478fb8fa0c90b64ec8a4
Terence Aqachmar <2087939a@student.gla.ac.uk>
2015-02-27 19:08:03 +0000

    Chapter 12 complete

--- rango/urls.py
+++ rango/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, url
 from rango import views
 from django.conf import settings # 5.3
 from django.conf.urls.static import static # 5.3
+from registration.backends.simple.views import RegistrationView
+
 
 urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
@@ -9,10 +11,7 @@ urlpatterns = patterns('',
         url(r'^add_category/$', views.add_category, name='add_category'), # NEW MAPPING!
 		url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
 		url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'), # 7.3.5
-		url(r'^register/$', views.register, name='register'), #  9.4.4
-		url(r'^login/$', views.user_login, name='login'), #9.5.3
 		url(r'^restricted/', views.restricted, name='restricted'), #9.6.1
-		url(r'^logout/$', views.user_logout, name='logout'), #9.7
 		)
 
 if not settings.DEBUG:
